325bf4e32bd04efd429a3d110ef6d2ae14092a94
Jukonti Mithun Reddy <21eg105e23@anurag.edu.in>
2025-03-06 22:37:46 +0530

    m

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { MComponent } from './m/m.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    MComponent
   ],
   imports: [
     BrowserModule,
